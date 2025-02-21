422af6c461448c2429168690ed60ecc9f5ad2ac9
lawpandit6 <lawpandit6@gmail.com>
2025-02-21 14:29:54 +0530

    second commit

---
+++ 01_variables&datatype/01_assignment/product.js
@@ -0,0 +1,8 @@
+const product={
+    title:"Ball Pen",
+    price:45.5,
+    rating:5,
+    dealOfTheDay:false,
+    off:5
+};
+console.log(product);
---
+++ 01_variables&datatype/02_assignment/profile.js
@@ -0,0 +1,8 @@
+let profile={
+    userName:"@law_pandit6",
+    followers:1000,
+    followings:245,
+    post:500
+};
+
+console.log(profile);
